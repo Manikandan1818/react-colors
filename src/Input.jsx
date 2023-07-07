ad858c46e34eff3bbf4d49e2b33edcd41d9d251b
Manikandan1818 <simbumanii18@gmail.com>
2023-07-07 20:45:10 +0530

    Using usestate and props

---
+++ src/Input.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import colorNames from "colornames";
+
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label>Add Color Name:</label>
+      <input
+        autoFocus
+        type="text"
+        placeholder="Add color name"
+        colorValue={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
+      />
+      <button
+        onClick={() => {
+          setIsDarkText(!isDarkText);
+        }}
+      >
+        Toggle Text Color
+      </button>
+    </form>
+  );
+};
+
+export default Input;
